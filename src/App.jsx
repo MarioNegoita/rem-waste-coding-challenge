36d0bae5be0f7eceba423c1d35d35aa64e8fc98d
MarioNegoita <marionegoita@gmail.com>
2025-03-22 12:55:23 +0200

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./css/index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-green-700 text-white font-bold text-3xl">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
